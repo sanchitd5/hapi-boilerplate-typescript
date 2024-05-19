b3c115d7bc59eb56dd269101e0058256031a2da0
Sanchit Dang <sanchitd5@live.com>
2024-05-19 17:14:34 +1000

    chore: Update AWS S3 credentials

--- src/config/awsS3Config.ts
+++ src/config/awsS3Config.ts
@@ -33,8 +33,8 @@ class S3BucketCredentials implements AwsS3BucketProperties {
     readonly projectFolder: string;
     readonly bucket: string;
     readonly endpoint = "s3.au-syd.cloud-object-storage.appdomain.cloud";
-    readonly apiKeyId = "mhNbtjQUlsq2LBh5F03g81g1Wcq8hN6H1ZrWnpRtcD3L";
-    readonly serviceInstanceId = "crn:v1:bluemix:public:cloud-object-storage:global:a/200d885c6c6a4629814c74e3c7594d35:bb53fed0-c301-4705-ad41-27a08a0ae3a6:bucket:ipan-v2-bucket";
+    readonly apiKeyId = "";
+    readonly serviceInstanceId = "";
     readonly folder: S3FolderDetails;
 
     constructor(data: S3BucketCredentialsProps) {
